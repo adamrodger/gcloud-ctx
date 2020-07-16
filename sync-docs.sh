cdd709417c994aa05fd769941d7af25d8cfdb2e6
Adam Rodger <adam.rodger@gmail.com>
2020-07-16 20:21:49 +0100

    Script for syncing docs and README

---
+++ sync-docs.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+if ! command -v cargo-sync-readme &> /dev/null
+then
+    echo "'cargo sync-readme' could not be found - install it with 'cargo install cargo-sync-readme"
+    exit
+fi
+
+pushd gcloud-ctx > /dev/null
+echo -n "Syncing gcloud-ctx... "
+cargo sync-readme
+echo "Done"
+popd > /dev/null
+
+pushd gctx > /dev/null
+echo -n "Syncing gctx... "
+cargo sync-readme -z
+echo "Done"
+popd > /dev/null
