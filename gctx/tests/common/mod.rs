ec3c0e7f2cbbd18ad549c17f5b2f603177dccb66
Adam Rodger <adam.rodger@gmail.com>
2025-11-19 21:01:28 +0000

    chore: fmt

--- gctx/tests/common/mod.rs
+++ gctx/tests/common/mod.rs
@@ -1,4 +1,4 @@
-use assert_cmd::{Command, cargo::cargo_bin_cmd};
+use assert_cmd::{cargo::cargo_bin_cmd, Command};
 use assert_fs::{prelude::*, TempDir};
 
 const CLOUDSDK_CONFIG: &str = "CLOUDSDK_CONFIG";
